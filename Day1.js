1f1ca784603898a5273b83f6757c6b5fb51fc0ec
Edw <edwd42@gmail.com>
2017-09-13 16:22:14 +0000

    add HackerRank challenge instructions and comments in my code

--- Day1.js
+++ Day1.js
@@ -77,15 +77,18 @@ function main() {
 
     // Declare second integer, double, and String variables.
     var i2, d2, s2;
+    
     // Read and save an integer, double, and String to your variables.
     i2 = readLine();
     d2 = readLine();
     s2 = readLine();
-        // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of both integer variables on a new line.
     console.log(i + parseInt(i2));
+    
     // Print the sum of the double variables on a new line.
-
     console.log((d + parseFloat(d2)).toFixed(1));
+    
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
     console.log(s + s2);   
--- Day2.js
+++ Day2.js
@@ -1,19 +1,65 @@
-// 'use strict';
+/*
+https://www.hackerrank.com/challenges/30-operators
 
-// var _input = '';
-// var _index = 0;
-// process.stdin.on('data', (data) => { _input += data; });
-// process.stdin.on('end', () => {
-// 	_input = _input.split(new RegExp('[\n ]+'));
-// 	main(+(_input[0]), +(_input[1]), +(_input[2]));    
-// });
+Day 2: Operators
+
+Objective 
+In this challenge, you'll work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result!
+
+Input Format
+
+There are  lines of numeric input: 
+The first line has a double,  (the cost of the meal before tax and tip). 
+The second line has an integer,  (the percentage of  being added as tip). 
+The third line has an integer,  (the percentage of  being added as tax).
+
+Output Format
+
+Print The total meal cost is totalCost dollars., where  is the rounded integer result of the entire bill ( with added tax and tip).
+
+Sample Input
+
+12.00
+20
+8
+Sample Output
+
+The total meal cost is 15 dollars.
+Explanation
+
+Given: 
+, , 
+
+Calculations: 
+
+We round  to the nearest dollar (integer) and then print our result:
+
+The total meal cost is 15 dollars.
+*/
+
+'use strict';
+
+var _input = '';
+var _index = 0;
+process.stdin.on('data', (data) => { _input += data; });
+process.stdin.on('end', () => {
+	_input = _input.split(new RegExp('[\n ]+'));
+	main(+(_input[0]), +(_input[1]), +(_input[2]));    
+});
 
 function main(mealCost, tipPercent, taxPercent) {
-        // Write your code here
+    
+// code above this line unchangeable
+// my code submission below
+
+    // Write your code here
     var totalCost = Math.round(mealCost + (mealCost*tipPercent/100) + (mealCost*taxPercent/100));
 
     // Use console.log() to print to stdout
     console.log("The total meal cost is " + totalCost + " dollars.");
 }
-
-main(12.00, 20, 8)
