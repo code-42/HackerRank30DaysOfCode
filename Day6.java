8245b90eef959b0e4801d459cced462f306aa765
Edw <edwd42@gmail.com>
2017-09-13 16:54:33 +0000

    complete Day 6 solution

--- Day6.java
+++ Day6.java
@@ -1,3 +1,47 @@
+/*
+https://www.hackerrank.com/challenges/30-review-loop
+
+Day 6: Let's Review
+
+Objective 
+Today we're expanding our knowledge of Strings and combining it with what we've already learned about loops. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given a string, , of length  that is indexed from  to , print its even-indexed and odd-indexed characters as  space-separated strings on a single line (see the Sample below for more detail).
+
+Note:  is considered to be an even index.
+
+Input Format
+
+The first line contains an integer,  (the number of test cases). 
+Each line  of the  subsequent lines contain a String, .
+
+Constraints
+
+Output Format
+
+For each String  (where ), print 's even-indexed characters, followed by a space, followed by 's odd-indexed characters.
+
+Sample Input
+
+2
+Hacker
+Rank
+Sample Output
+
+Hce akr
+Rn ak
+Explanation
+
+Test Case 0:  
+
+The even indices are , , and , and the odd indices are , , and . We then print a single line of  space-separated strings; the first string contains the ordered characters from 's even indices (), and the second string contains the ordered characters from 's odd indices ().
+
+Test Case 1:  
+
+The even indices are  and , and the odd indices are  and . We then print a single line of  space-separated strings; the first string contains the ordered characters from 's even indices (), and the second string contains the ordered characters from 's odd indices ().
+*/
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -5,6 +49,10 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+// code above this line unchangeable
+// my code submission below
+
         Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
         sc.nextLine();
