0e8fe476e7c82a1d913aa84e765243316bd5c5ae
Edw <edwd42@gmail.com>
2017-09-13 16:16:18 +0000

    add HackerRank challenge instructions and comments in my code

--- Day1.js
+++ Day1.js
@@ -1,51 +1,93 @@
-// process.stdin.resume();
-// process.stdin.setEncoding('ascii');
+/*
+https://www.hackerrank.com/challenges/30-data-types
 
-// var input_stdin = "";
-// var input_stdin_array = "";
-// var input_currentline = 0;
+Day 1: Data Types
 
-// process.stdin.on('data', function (data) {
-//     input_stdin += data;
-// });
+Objective 
+Today, we're discussing data types. Check out the Tutorial tab for learning materials and an instructional video!
 
-// process.stdin.on('end', function () {
-//     input_stdin_array = input_stdin.split("\n");
-//     main();    
-// });
+Task 
+Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
 
-// // Reads complete line from STDIN
-// function readLine() {
-//     return input_stdin_array[input_currentline++];
-// }
+Declare  variables: one of type int, one of type double, and one of type String.
+Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+Use the  operator to perform the following operations: 
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line.
+Note: If you are using a language that doesn't support using  for string concatenation (e.g.: C), you can just print one variable immediately following the other on the same line. The string provided in your editor must be printed first, immediately followed by the string you read as input.
 
-var input_stdin_array = [12,4.0,"is the best place to learn and practice coding!"];
+Input Format
+
+The first line contains an integer that you must sum with . 
+The second line contains a double that you must sum with . 
+The third line contains a string that you must concatenate with .
+
+Output Format
+
+Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
+
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+Sample Output
+
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+Explanation
+
+When we sum the integers  and , we get the integer . 
+When we sum the floating-point numbers  and , we get . 
+When we concatenate HackerRank with is the best place to learn and practice coding!, we get HackerRank is the best place to learn and practice coding!.
+
+You will not pass this challenge if you attempt to assign the Sample Case values to your variables instead of following the instructions above and reading input from stdin.
+*/
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+// Reads complete line from STDIN
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
 
 function main() {
     var i = 4
     var d = 4.0
     var s = "HackerRank "
     
-    
+// code above this line unchangeable
+// my code submission below
+
     // Declare second integer, double, and String variables.
-var i2, d2, s2;
+    var i2, d2, s2;
     // Read and save an integer, double, and String to your variables.
-// i2 = readLine();
-// d2 = readLine();
-// s2 = readLine();
-
-i2 = input_stdin_array[0];
-d2 = input_stdin_array[1];
-s2 = input_stdin_array[2];
-
-    // Print the sum of both integer variables on a new line.
-console.log(i + parseInt(i2));
+    i2 = readLine();
+    d2 = readLine();
+    s2 = readLine();
+        // Print the sum of both integer variables on a new line.
+    console.log(i + parseInt(i2));
     // Print the sum of the double variables on a new line.
-console.log((d + parseFloat(d2)).toFixed(1));
+
+    console.log((d + parseFloat(d2)).toFixed(1));
     // Concatenate and print the String variables on a new line
     // The 's' variable above should be printed first.
-console.log(s + s2);    
-
+    console.log(s + s2);   
+    
 }
-
-main(input_stdin_array)
