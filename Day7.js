e372135036afbe1d3fd1a3464affb1428b46e1d2
Edw <edwd42@gmail.com>
2017-09-24 03:19:18 +0000

    complete Day 7 solution in javascript

---
+++ Day7.js
@@ -0,0 +1,79 @@
+/*
+
+https://www.hackerrank.com/challenges/30-arrays/problem
+
+HackerRank 30 Days of Code  
+Day 7: Arrays 
+by saikiran9194
+Problem
+Submissions
+Leaderboard
+Discussions
+Editorial
+Tutorial
+Objective 
+Today, we're learning about the Array data structure. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+Given an array, , of  integers, print 's elements in reverse order as a single line of space-separated numbers.
+
+Input Format
+
+The first line contains an integer,  (the size of our array). 
+The second line contains  space-separated integers describing array 's elements.
+
+Constraints
+
+, where  is the  integer in the array.
+Output Format
+
+Print the elements of array  in reverse order as a single line of space-separated numbers.
+
+Sample Input
+
+4
+1 4 3 2
+Sample Output
+
+2 3 4 1
+Submissions: 114711
+Max Score: 30
+Difficulty: Easy
+Rate This Challenge:
+    
+More
+Current Buffer (saved locally, editable)     
+ 
+JavaScript (Node.js)
+*/
+
+ 
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    console.log(arr.reverse().join(' '));
+}
+
